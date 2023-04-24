17cfd3f05a6c50cde5ce212bff014502c2c4993d
Maggie <maggie@Examle.com>
2023-04-24 10:57:31 -0700

    save

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/**
- *main - Entry point
+/ **
+ * main - Entry point
  *
- *Description AC program that prints with printf function
+ * Description AC program that prints with printf function
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
  int main(void)
 { 
