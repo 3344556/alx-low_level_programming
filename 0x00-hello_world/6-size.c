4536478b266920e3252cf41c158d63006c863850
Maggie <maggie@Examle.com>
2023-04-24 11:44:15 -0700

    save

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description using size of to print the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{ 
+printf ( "Size of a char ,% lu bytes (s) ", size of (char));
+ printf ( "Size of an int ,% lu bytes (s) ", size of (int));
+printf ( "Size of a long int ,% lu bytes (s) ", size of (long int));
+printf ( "Size of a long long int ,% lu bytes (s) ", size of (long long int));
+printf ( "Size of a float ,% lu bytes (s) ", size of (a float));
+return (0);
+}
