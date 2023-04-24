bbcf5ee7319c895b5751dae818a0bf613623ac18
Maggie <maggie@Examle.com>
2023-04-24 09:33:25 -0700

    save

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description AC program that prints with putsfunction 
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{ 
+ puts("\"Programming is like building a multilingual puzzle") ;
+return (0) ;
+}
+
+
