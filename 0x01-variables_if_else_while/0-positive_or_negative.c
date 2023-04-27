2e5df193a01dbcaff5ab84a12edce4a0bd4bf9a3
Maggie <maggie@Examle.com>
2023-04-26 21:05:11 -0700

    save

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,22 @@
 #include <time.h>
 #include<studio.h>
 /**
- * main - determines if number is greater than or less than or equal to zero
+ * main - determines if number is greater than or less than or equal to zero 
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-int n; 	srand(time(0));
-n = rand() - RAND_MAX / 2;
+{	
+int n; 	srand(time(0));	
+n = rand() - RAND_MAX / 2;	
 if (n > 0)
 {
-print("%d is positive \n", n);
+printf("%d is positive \n", n);
 }
 else if (n == 0)
-print("%d is zero\n", n)
+printf("%d is zero\n", n);
 }
 else
-print("%d is negative\n", n)
+printf("%d is negative\n", n);
 	return(0);
 }
